7d67135e7c1c5b3370d79793b75245a170a9b805
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2024-02-05 15:38:26 -0300

    Update Adicionando coluna - 10% de desconto.sql

--- Adicionando coluna - 10% de desconto.sql
+++ Adicionando coluna - 10% de desconto.sql
@@ -1,8 +1,8 @@
-#seleciono as colunas customer_id e amount
-# faço o calculo reduzindo o valor do proprio valor * 10 (entre parenteses para multiplicar primeiro)
-# AS é para CRIAR O TEXTO TITULO '10% de desconto'
-# from da tabela payment
-# where será onde customer_id for igual a 1
+--#seleciono as colunas customer_id e amount
+--# faço o calculo reduzindo o valor do proprio valor * 10 (entre parenteses para multiplicar primeiro)
+--# AS é para CRIAR O TEXTO TITULO '10% de desconto'
+--# from da tabela payment
+--# where será onde customer_id for igual a 1
 
 select customer_id, amount,
 amount - (amount * 0.10) AS '10% de desconto'

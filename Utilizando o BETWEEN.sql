c2964cfdff76385b4e907fb5458966173ce41a10
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2024-02-05 15:35:55 -0300

    Update Utilizando o BETWEEN.sql

--- Utilizando o BETWEEN.sql
+++ Utilizando o BETWEEN.sql
@@ -1,7 +1,7 @@
-# UTILIZANDO O BETWEEN
-# selecionando toda a tabela payment
-# onde amount é igual ou maior que 1.99 E menor ou igual a 3.99
-#vantagens do between é um codigo mais limpo
+--# UTILIZANDO O BETWEEN
+--# selecionando toda a tabela payment
+--# onde amount é igual ou maior que 1.99 E menor ou igual a 3.99
+--#vantagens do between é um codigo mais limpo
 
 select * from payment
 where amount between 1.99 AND 3.99

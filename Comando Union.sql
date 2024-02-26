cc3691f1263ec165561c3287a93b73a300dab72a
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2024-02-26 11:26:01 -0300

    Create Comando Union.sql

---
+++ Comando Union.sql
@@ -0,0 +1,12 @@
+-- Uniao simples de duas tabelas
+-- union all (QUANDO VOCE SABE QUE NAO EXISTE VALORES DUPLICADOS)
+-- union (CASO EXISTA VALORES DUPLICADOS)
+
+
+select * from venda_produtos
+union
+select * from produtos
+
+
+
+-- depois é so copiar o resultado e colocar na planilha do excel.

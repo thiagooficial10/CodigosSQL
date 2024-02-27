116dffc973151ede375e6893ee6d23279952f1af
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2024-02-27 16:14:51 -0300

    Create Convertendo texto em numero.sql

---
+++ Convertendo texto em numero.sql
@@ -0,0 +1,4 @@
+-- conversao de texto em numero (mesma coisa do texto em data)
+
+-- subtração de 100 - 10 sem passar tabela ou coluna, transformando o texto em numero
+select '100'::numeric - '10'::numeric

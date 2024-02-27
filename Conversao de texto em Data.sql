d0d6f4aaea4327874b1e927730833b134c8b1d5c
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2024-02-27 16:18:37 -0300

    Update Conversao de texto em Data.sql

--- Conversao de texto em Data.sql
+++ Conversao de texto em Data.sql
@@ -1,4 +1,5 @@
 -- conversao de texto em data utilizando operador ::
+-- tambem é possivel utilizar o CAST
 
 -- selecionando direto a data sem passar pela coluna e tabela
 select '2021-10-01'::date - '2021-02-01'::date
@@ -7,3 +8,7 @@ select '2021-10-01'::date - '2021-02-01'::date
 -- selecionando a coluna e a tabela e transformando a coluna de texto em data
 select nome_coluna::date
 from nome_tabela
+
+
+-- Utilizando o CAST
+select cast ('2021-10-01' as date) -  cast('2021-02-01' as date)

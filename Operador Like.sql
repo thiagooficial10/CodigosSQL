8b1ca32f09fe887739fb7f8c6216ec3da9529e9e
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2025-03-27 09:56:14 -0300

    Update Operador Like.sql

--- Operador Like.sql
+++ Operador Like.sql
@@ -5,11 +5,16 @@
 select * from actor
 where first_name LIKE 'AL%'
 
-
-
 --# NOVO EXEMPLO LIKE
 --#buscando toda a tabela de actor
 --#onde o first_name precisa FINALIZAR com a LETRA AL
 
 select * from actor
 where first_name LIKE '%AL'
+
+-- SELECIONA A TABELA CUSTOMERS, PEGA A COLUNA FIRST_NAME, NOME QUE COMEÇA COM LETRA 'M' E TERMINA COM 'O'
+select * from teste.customers
+where first_name like 'M%' and first_name like '%o'
+--OUTRA OPÇÃO É ESSE ABAIXO, FAZ A MESMA COISA
+select * from teste.customers
+where first_name like 'M%o' 

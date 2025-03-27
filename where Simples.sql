d445ed8ce56d4bed746d7c99cbd64e9185b07bfa
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2025-03-27 10:18:09 -0300

    Update where Simples.sql

--- where Simples.sql
+++ where Simples.sql
@@ -5,3 +5,9 @@
 select actor_id, last_name, first_name from actor 
 where actor_id = 100 
 order by last_name 
+
+
+-- UTILIZANDO WHERE COM AND LIMITANDO A 100 LINHAS
+SELECT *  FROM `bigquery-public-data.chicago_crime.crime` 
+where primary_type = 'CRIM SEXUAL ASSAULT' and description = 'PREDATORY' and location_description = 'VEHICLE NON-COMMERCIAL'
+LIMIT 100

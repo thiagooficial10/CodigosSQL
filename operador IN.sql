c12eee6071f726ea4cfbe73599110ae8b8ed35ba
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2025-03-27 10:00:48 -0300

    Update operador IN.sql

--- operador IN.sql
+++ operador IN.sql
@@ -2,6 +2,10 @@
 --# selecionando toda a tabela address
 --# onde distrito tem que ser igual a alberta, texas e california
 
-
 select * from address
-where district IN ('alberta', 'texas', 'california')
+where district IN ('alberta', 'texas', 'california');
+
+
+-- NOVO EXEMPLO
+select * from teste.customers
+where estado in ('Acre', 'Minas Gerais', 'Bahia');

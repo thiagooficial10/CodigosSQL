774325d759c34df34d3f1c46224e53768bd9ddf1
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2025-03-31 17:49:25 -0300

    Create LENGTH.sql

---
+++ LENGTH.sql
@@ -0,0 +1,21 @@
+/*8) DESAFIO: No mesmo dataset anterior, descubra o nome dos times que tenham 
+somente 6 caracteres, usando o like(PROFESSOR PROVAVELMENTE ERROU, DEVE UTILIZAR O LENGHT). Retorne de forma única.*/
+
+SELECT distinct homeTeamName, awayTeamName
+FROM `bigquery-public-data.baseball.schedules`
+where length(homeTeamName) = 6;
+
+
+-- CASO EU QUEIRA UNIR DUAS TABELAS DE NOMES, POR EXEMPLO TIME DA CASA E TIME VISITANTE, E VER OS NOMES DISTINTOS COM 6 CARACTERES.
+SELECT DISTINCT time_nome
+FROM (
+    SELECT homeTeamName AS time_nome  
+    FROM `bigquery-public-data.baseball.schedules`
+    WHERE LENGTH(homeTeamName) = 6
+  
+    UNION ALL
+  
+    SELECT distinct awayTeamName AS time_nome  
+    FROM `bigquery-public-data.baseball.schedules`
+    WHERE LENGTH(awayTeamName) = 6
+) AS times_combinados;
